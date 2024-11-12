4e61ed2b2af1954ee2532e215cf7de388d6d72df
dawrentz <156712569+dawrentz@users.noreply.github.com>
2024-11-12 12:02:43 -0600

    Initialize project.
    Setup and/or connect:
    -html
    -css
    -index.js
    -webpack files
    -json
    -install npm and dependencies
    -install esling and prettier
    -check make sure runs (dev and prod)

--- src/index.js
+++ src/index.js
@@ -1,29 +1,5 @@
 //index.js
 
 import "./style.css";
-import * as renderModule from "./renderModule.js";
-import * as eventListenerModule from "./eventListenerModuel.js";
-import * as localStorageModule from "./localStorageModule.js";
 
-//declarations
-const addProjectBtn = document.querySelector("#add-project-btn");
-const demoBtn = document.querySelector("#demo-btn");
-
-//============================================ test ============================================//
-
-//optional for testing
-// localStorage.clear();
-// console.table(localStorage);
-// eventListenerModule.seeAllClickEventTargets();
-
-//============================================ init ============================================//
-
-//add EL to HTML btns
-eventListenerModule.addELtoSidebarAddProjBtn(addProjectBtn);
-eventListenerModule.addELtoDemoBtn(demoBtn);
-
-//load localStorage data
-localStorageModule.loadLocalStorageToApp();
-
-//run
-renderModule.renderAll();
+console.log("hello");
